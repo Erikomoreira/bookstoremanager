0e4728b42381711e7d362409d63d123d79eb87c4
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-03-10 00:27:31 -0300

    Created model mappings

---
+++ src/main/java/com/erik/bookstoremanager/books/entity/Book.java
@@ -0,0 +1,39 @@
+package com.erik.bookstoremanager.books.entity;
+
+import com.erik.bookstoremanager.author.entity.Author;
+import com.erik.bookstoremanager.publishers.entity.Publisher;
+import com.erik.bookstoremanager.users.entity.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "pages", columnDefinition = "integer default 0")
+    private int pages;
+
+    @Column(name = "chapters", columnDefinition = "integer default 0")
+    private int chapters;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Author author;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Publisher publisher;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private User user;
+
+}

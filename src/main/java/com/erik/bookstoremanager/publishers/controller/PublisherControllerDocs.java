8c98aeefac2c62ecd12115e6760cae6d974569a2
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-03-20 21:18:06 -0300

    Developed structure classes for publishers module

---
+++ src/main/java/com/erik/bookstoremanager/publishers/controller/PublisherControllerDocs.java
@@ -0,0 +1,8 @@
+package com.erik.bookstoremanager.publishers.controller;
+
+import io.swagger.annotations.Api;
+
+@Api("Publishers management")
+public class PublisherControllerDocs {
+
+}
---
+++ src/test/java/com/erik/bookstoremanager/publishers/service/PublisherServiceTest.java
@@ -0,0 +1,29 @@
+package com.erik.bookstoremanager.publishers.service;
+
+import com.erik.bookstoremanager.publishers.builder.PublisherDTOBuilder;
+import com.erik.bookstoremanager.publishers.mappers.PublisherMapper;
+import com.erik.bookstoremanager.publishers.repository.PublisherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PublisherServiceTest {
+
+    private final PublisherMapper publisherMapper = PublisherMapper.INSTANCE;
+
+    @Mock
+    private PublisherRepository publisherRepository;
+
+    @InjectMocks
+    private PublisherService publisherService;
+
+    private PublisherDTOBuilder publisherDTOBuilder;
+
+    @BeforeEach
+    void setUp() {
+        publisherDTOBuilder = PublisherDTOBuilder.builder().build();
+    }
+}

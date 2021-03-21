703cc545d80a93caec36e0025a46b0a708b3f60c
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-03-21 14:02:26 -0300

    Developed publisher creation feature

--- src/test/java/com/erik/bookstoremanager/publishers/service/PublisherServiceTest.java
+++ src/test/java/com/erik/bookstoremanager/publishers/service/PublisherServiceTest.java
@@ -1,13 +1,25 @@
 package com.erik.bookstoremanager.publishers.service;
 
 import com.erik.bookstoremanager.publishers.builder.PublisherDTOBuilder;
+import com.erik.bookstoremanager.publishers.dto.PublisherDTO;
+import com.erik.bookstoremanager.publishers.entity.Publisher;
+import com.erik.bookstoremanager.publishers.exception.PublisherAlreadyExistsException;
 import com.erik.bookstoremanager.publishers.mappers.PublisherMapper;
 import com.erik.bookstoremanager.publishers.repository.PublisherRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PublisherServiceTest {
@@ -26,4 +38,30 @@ public class PublisherServiceTest {
     void setUp() {
         publisherDTOBuilder = PublisherDTOBuilder.builder().build();
     }
+
+    @Test
+    void whenNewPublisherIsInformedThenItShouldBeCreated() {
+        PublisherDTO expectedPublisherToCreateDTO = publisherDTOBuilder.buildPublisherDTO();
+        Publisher expPublisherCreated = publisherMapper.toModel(expectedPublisherToCreateDTO);
+
+        when(publisherRepository.findByNameOrCode(expectedPublisherToCreateDTO.getName(), expectedPublisherToCreateDTO.getCode()))
+                .thenReturn(Optional.empty());
+        when(publisherRepository.save(expPublisherCreated)).thenReturn(expPublisherCreated);
+
+        PublisherDTO createdPublisherDTO = publisherService.create(expectedPublisherToCreateDTO);
+
+        assertThat(createdPublisherDTO, is(equalTo(expectedPublisherToCreateDTO)));
+    }
+
+    @Test
+    void whenExistingPublisherIsInformedThenAnExceptionShouldBeThrown() {
+        PublisherDTO expectedPublisherToCreateDTO = publisherDTOBuilder.buildPublisherDTO();
+        Publisher expectedPublisherDuplicated = publisherMapper.toModel(expectedPublisherToCreateDTO);
+
+        when(publisherRepository.findByNameOrCode(expectedPublisherToCreateDTO.getName(), expectedPublisherToCreateDTO.getCode()))
+                .thenReturn(Optional.of(expectedPublisherDuplicated));
+
+        assertThrows(PublisherAlreadyExistsException.class, () -> publisherService.create(expectedPublisherToCreateDTO));
+    }
+
 }

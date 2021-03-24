fec2385e01799c6712aeaed54681e0b4f5f35234
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-03-24 14:05:11 -0300

    Developed user creation feature

--- src/main/java/com/erik/bookstoremanager/users/service/UserService.java
+++ src/main/java/com/erik/bookstoremanager/users/service/UserService.java
@@ -1,9 +1,15 @@
 package com.erik.bookstoremanager.users.service;
 
+import com.erik.bookstoremanager.users.dto.MessageDTO;
+import com.erik.bookstoremanager.users.dto.UserDTO;
+import com.erik.bookstoremanager.users.entity.User;
+import com.erik.bookstoremanager.users.exception.UserAlreadyExistsException;
 import com.erik.bookstoremanager.users.mapper.UserMapper;
 import com.erik.bookstoremanager.users.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,9 +19,30 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    public MessageDTO create(UserDTO userCreateDTO) {
+        verifyIfExists(userCreateDTO.getEmail(), userCreateDTO.getUsername());
+        User userToCreate = userMapper.toModel(userCreateDTO);
+        User createdUser = userRepository.save(userToCreate);
+        return creationMessage(createdUser);
+    }
 
+    private MessageDTO creationMessage(User createdUser) {
+        String createdUserName = createdUser.getUsername();
+        Long createdId = createdUser.getId();
+        String createdUserMessage = String.format("User %s with ID %s successfully created", createdUserName, createdId);
+        return MessageDTO.builder()
+                .message(createdUserMessage)
+                .build();
+    }
+
+    private void verifyIfExists(String email, String username) {
+        Optional<User> foundUser = userRepository.findByEmailOrUsername(email, username);
+        if (foundUser.isPresent()) {
+            throw new UserAlreadyExistsException(email, username);
+        }
+    }
 }

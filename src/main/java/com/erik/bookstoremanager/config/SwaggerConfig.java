c3325f3f539dd76db336bbc8c73dd630709f5ed2
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-04-03 23:59:35 -0300

    Updated swagger configuration

--- src/main/java/com/erik/bookstoremanager/config/SwaggerConfig.java
+++ src/main/java/com/erik/bookstoremanager/config/SwaggerConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,19 +22,21 @@ public class SwaggerConfig {
     private static final String API_TITLE = "Bookstore Manager";
     private static final String API_DESCRIPTION = "Bookstore Manager API";
     private static final String API_VERSION = "1.0.0";
-    private static final String CONTACT_NAME ="Erik Moreira";
-    private static final String CONTACT_EMAIL="erik.oliveira.moreira@hotmail.com";
+    private static final String CONTACT_NAME = "Erik Moreira";
+    private static final String CONTACT_EMAIL = "erik.oliveira.moreira@hotmail.com";
     private static final String CONTACT_GITHUB = "https://github.com/Erikomoreira";
 
     @Bean
-    public Docket api(){
+    public Docket api() {
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(buildApiInfo());
+                .apiInfo(buildApiInfo())
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()));
     }
 
     private ApiInfo buildApiInfo() {
@@ -41,5 +46,19 @@ public class SwaggerConfig {
                 .version(API_VERSION)
                 .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/.*")).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return Collections.singletonList(new SecurityReference("Bearer", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
     }
 }
